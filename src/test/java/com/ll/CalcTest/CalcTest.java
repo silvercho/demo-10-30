44029aa40b692e463abeb08368ac72926b65f191
silvercho <tjddms01212@gmail.com>
2023-10-30 11:07:28 +0900

    test t2 추가

--- src/test/java/com/ll/CalcTest/CalcTest.java
+++ src/test/java/com/ll/CalcTest/CalcTest.java
@@ -3,11 +3,18 @@ package com.ll.CalcTest;
 import com.ll.Calc;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
+import static org.assertj.core.api.Assertions.assertThat;
 public class CalcTest {
     @Test
     @DisplayName("Calc에 run 메서드는 int 를 리턴한다.")
     void t1(){
         int rs = Calc.run("");
+    }
+
+    @Test
+    @DisplayName("10+10 = 20")
+    void t2() {
+        int rs = Calc.run("10+10");
+        assertThat(rs).isEqualTo(20);
     }
 }
